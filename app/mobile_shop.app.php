db4ae437084d914fa7b975ac5f7bb3f023f6a2f8
Connor Weng <foamtea30@gmail.com>
2016-10-10 00:48:21 +0800

    update mobile_shop app

--- app/mobile_shop.app.php
+++ app/mobile_shop.app.php
@@ -7,15 +7,20 @@ class Mobile_shopApp extends Mobile_frontendApp {
     function index() {
         $mk_id = $this->_make_sure_numeric('mk_id', 0);
         $page = $this->_make_sure_numeric('page', 1);
-        $this->_index($mk_id, $page);
+        $keywords = isset($_REQUEST['keywords']) ? $_REQUEST['keywords'] : '';
+        $this->_index($mk_id, $page, $keywords);
     }
 
     // 根据市场、排序方式、页面大小、页面号进行店铺查询
-    function _index($mk_id, $page) {
+    function _index($mk_id, $page, $keywords) {
         $order_by = 'mk_id, floor, address';
         $page_per = 25;
         $page = $this->_get_page($page_per);
-        $conditions = $mk_id == 0 ? '' : "mk_id in (select mk_id from ecm_market where parent_id = {$mk_id}) or mk_id = {$mk_id}";
+        $conditions = '';
+        if ($keywords) {
+            $conditions .= "(store_name like '%".trim($keywords)."%' OR dangkou_address like '%".trim($keywords)."%' OR address like '%".trim($keywords)."%')";
+        }
+        $conditions .= $mk_id == 0 ? '' : " and (mk_id in (select mk_id from ecm_market where parent_id = {$mk_id}) or mk_id = {$mk_id})";
         $shop_mod =& m('store');
         $shop_list = $shop_mod->find(array(
             'fields' => 'mk_id, mk_name, store_id, floor, address, store_name, see_price, business_scope',
