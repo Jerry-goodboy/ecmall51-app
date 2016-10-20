f1b33e5c789d5eccc2cbc52065970441e6bb3b5f
Connor Weng <foamtea30@gmail.com>
2016-10-21 00:08:59 +0800

    update mobile_home app

--- app/mobile_home.app.php
+++ app/mobile_home.app.php
@@ -2,13 +2,13 @@
 
 class Mobile_homeApp extends MallbaseApp {
     function index() {
-        $order_by = 'add_time DESC';
+        $order_by = 'goods_id DESC';
         $goods_mod =& m('goods');
         $goods_list = $goods_mod->find(array(
             'fields' => 'goods_id, goods_name, default_image, price, store_id',
             'index_key' => false,
             'order' => $order_by,
-            'limit' => 20));
+            'limit' => '1000, 20'));
         $this->assign('goods_list', $goods_list);
         $this->display('mobile_home.html');
     }
