106bdaaf6fed98d5d5f92aa1b426407ead78914a
Connor Weng <foamtea30@gmail.com>
2016-12-01 21:59:55 +0800

    add support for taobao login

--- app/mobile_member.app.php
+++ app/mobile_member.app.php
@@ -50,6 +50,53 @@ class Mobile_memberApp extends Mobile_frontendApp {
     function _generate_access_token() {
         return md5(uniqid()).md5(uniqid());
     }
+
+    function check() {
+        if(IS_POST) {
+            $taobao_username = $this->_make_sure_string('taobao_username', 50, '');
+            $id = $this->_make_sure_string('id', 255, '');
+            $nick = $this->_make_sure_string('nick', 255, '');
+            $avatar_url = $this->_make_sure_string('avatar_url', 255, '');
+            $authorization_code = $this->_make_sure_string('authorization_code', 255, '');
+            if (empty($taobao_username) ||
+                empty($id) ||
+                empty($nick) ||
+                empty($avatar_url) ||
+                empty($authorization_code)) {
+                $this->_ajax_error(400, PARAMS_ERROR, '参数错误');
+                return ;
+            }
+            $this->_check($taobao_username, $id, $nick, $avatar_url, $authorization_code);
+        } else {
+            $this->_ajax_error(400, NOT_POST_ACTION, 'not a post action');
+        }
+    }
+
+    function _check($taobao_username, $id, $nick, $avatar_url, $authorization_code) {
+        $auth_mod =& m('memberauth');
+        $auth_info = $auth_mod->get(array(
+            'conditions' => "vendor = 0 and vendor_user_nick = '{$taobao_username}'"));
+        if (empty($auth_info)) {
+            $this->_ajax_error(400, CHECK_TAOBAO_FAILED, '系统中不存在该淘宝用户，请前往www.51zwd.com使用一次淘登录');
+            return ;
+        }
+
+        $result = $auth_mod->edit($auth_info['user_id'], array(
+            'avatar_url' => $avatar_url,
+            'confusing_nick' => $nick,
+            'confusing_id' => $id,
+            'authorization_code' => $authorization_code));
+        if (!$result) {
+            $this->_ajax_error(500, DB_ERROR, '更新登录信息失败');
+        }
+
+        $ms =& ms();
+        $user_api = $ms->user;
+        $mod_user =& m('member');
+        $mod_access_token =& m('access_token');
+        $username = '51t_'.$taobao_username;
+        $this->_login($username, FIXED_PASSWORD, $user_api, $mod_user, $mod_access_token);
+    }
 }
 
 ?>
---
+++ sql/table_ecm_member_auth.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `ecm_member_auth` ADD `avatar_url` VARCHAR(255) NULL DEFAULT NULL;
+ALTER TABLE `ecm_member_auth` ADD `confusing_nick` VARCHAR(255) NULL DEFAULT NULL;
+ALTER TABLE `ecm_member_auth` ADD `confusing_id` VARCHAR(255) NULL DEFAULT NULL;
+ALTER TABLE `ecm_member_auth` ADD `authorization_code` VARCHAR(255) NULL DEFAULT NULL;
