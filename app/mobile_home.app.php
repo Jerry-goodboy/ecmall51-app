c71d72b2b0b36f61b6164e8be550ed7649d145ec
Connor Weng <foamtea30@gmail.com>
2017-01-02 02:35:19 +0800

    send shop info as well

--- app/mobile_home.app.php
+++ app/mobile_home.app.php
@@ -5,8 +5,9 @@ class Mobile_homeApp extends MallbaseApp {
         $order_by = 'goods_id';
         $goods_mod =& m('goods');
         $goods_list = $goods_mod->find(array(
-            'conditions' => 'default_spec != 0 and description is not null',
-            'fields' => 'goods_id, goods_name, default_image, price, store_id',
+            'conditions' => 'g.default_spec != 0 and g.description is not null',
+            'fields' => 'g.goods_id, g.goods_name, g.default_image, g.price, s.store_id, s.store_name, s.see_price, s.mk_name, s.address, s.business_scope',
+            'join' => 'belongs_to_store',
             'index_key' => false,
             'order' => $order_by,
             'limit' => '5000, 20'));
