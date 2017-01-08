2813531c0c2de67fc9c30cb96605a4dafb43a41f
Connor Weng <foamtea30@gmail.com>
2017-01-08 12:55:36 +0800

    state of ecm_member_auth should be 1 in order to change appkey

--- app/mobile_member.app.php
+++ app/mobile_member.app.php
@@ -75,7 +75,7 @@ class Mobile_memberApp extends Mobile_frontendApp {
     function _check($taobao_username, $id, $nick, $avatar_url, $authorization_code) {
         $auth_mod =& m('memberauth');
         $auth_info = $auth_mod->get(array(
-            'conditions' => "vendor = 0 and vendor_user_nick = '{$taobao_username}'"));
+            'conditions' => "state = 1 and vendor = 0 and vendor_user_nick = '{$taobao_username}'"));
         if (empty($auth_info)) {
             $this->_ajax_error(400, CHECK_TAOBAO_FAILED, '系统中不存在该淘宝用户，请前往www.51zwd.com使用一次淘登录');
             return ;
